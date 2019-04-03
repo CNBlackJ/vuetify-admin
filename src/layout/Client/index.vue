8665b5475c815ed2114c35a9aad29b3273bdac09
CNBlackJ <yes.heng@icloud.com>
2019-04-03 17:35:26 +0800

    feat: add client template

---
+++ src/layout/Client/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Navbar />
+
+    <Main />
+
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import main from './main'
+import navbar from './navbar'
+import sidebar from './sidebar'
+import footer from './footer'
+
+export default {
+  components: {
+    Main: main,
+    Navbar: navbar,
+    Sidebar: sidebar,
+    Footer: footer
+  }
+}
+</script>
---
+++ src/layout/Client/navbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    I am navbar
+  </div>
+</template>
