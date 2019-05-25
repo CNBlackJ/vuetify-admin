a1f31091ad6a7326c31c89883a2997dd3d3a499e
CNBlackJ <yes.heng@icloud.com>
2019-05-25 14:44:02 +0800

    feat: update product card action layout

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -24,14 +24,19 @@
 
         <v-container class="pt-0">
           <v-layout row wrap>
-            <v-btn small icon flat class="hidden-sm-and-down">
-              <v-icon>share</v-icon>
-            </v-btn>
-            <v-btn small icon flat @click="onAddToCart">
-              <v-icon>add_shopping_cart</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn small @click="onInquiry" color="primary" dark>Inquiry</v-btn>
+            <v-flex class="hidden-sm-and-down pa-0" md3 lg3 xl3>
+              <v-btn small icon flat>
+                <v-icon>share</v-icon>
+              </v-btn>
+            </v-flex>
+            <v-flex class="pa-0" xs3 sm3 md3 lg3 xl3>
+              <v-btn small icon flat @click="onAddToCart" class="mr-0 ml-0">
+                <v-icon>add_shopping_cart</v-icon>
+              </v-btn>
+            </v-flex>
+            <v-flex class="pa-0" xs9 sm9 md6 lg6 xl6>
+              <v-btn small @click="onInquiry" color="primary" dark>Inquiry</v-btn>
+            </v-flex>
           </v-layout>
         </v-container>
       </v-card>
