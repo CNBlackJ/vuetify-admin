dc3fa9215a66894954bc761fcd3e6540469850bf
CNBlackJ <yes.heng@icloud.com>
2019-05-31 00:10:48 +0800

    fix role config

--- server/config/role.js
+++ server/config/role.js
@@ -1,4 +1,4 @@
 module.exports = app => {
   app.role.use('admin', ctx => ctx.state.user && ctx.state.user.isAdmin)
-  app.role.use('user', ctx => ctx.user)
+  app.role.use('user', ctx => ctx.state.user)
 }
